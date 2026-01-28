f6081a70ed25b383f3d52ede01bec7701820126a
nobel-von-it <maksimdavydenko12@gmail.com>
2026-01-28 22:10:24 +0700

    feat(blog-platform-api): update, select one

--- blog-platform-api/blog-backend/src/main.rs
+++ blog-platform-api/blog-backend/src/main.rs
@@ -85,6 +85,45 @@ impl SqliteService {
         Ok(result)
     }
 
+    async fn select_post(&self, id: u64) -> Result<DBBlogPost> {
+        let conn = self.pool.get().await?;
+
+        let result = conn
+            .interact(move |conn| -> Result<DBBlogPost> {
+                let mut stmt = conn.prepare("SELECT * FROM posts WHERE id = ?1")?;
+                let post_iter = stmt.query_map([id], |row| {
+                    let id = row.get(0)?;
+                    let title = row.get(1)?;
+                    let content = row.get(2)?;
+                    let category = row.get(3)?;
+                    let tags = row.get(4)?;
+                    let created_at = row.get(5)?;
+                    let updated_at = row.get(6)?;
+
+                    Ok(RawDBBlogPost {
+                        id,
+                        title,
+                        content,
+                        category,
+                        tags,
+                        created_at,
+                        updated_at,
+                    })
+                })?;
+
+                DBBlogPost::any_from(
+                    post_iter
+                        .into_iter()
+                        .next()
+                        .ok_or(anyhow::anyhow!("post not found"))??,
+                )
+            })
+            .await
+            .map_err(|e| anyhow::anyhow!("interact error: {}", e))??;
+
+        Ok(result)
+    }
+
     async fn select_all_posts(&self) -> Result<Vec<DBBlogPost>> {
         let conn = self.pool.get().await?;
 
@@ -122,6 +161,38 @@ impl SqliteService {
             .into_iter()
             .map(DBBlogPost::any_from)
             .collect::<Result<Vec<DBBlogPost>>>()
+    }
+
+    async fn update_post(&self, id: u64, post: BlogPost) -> Result<DBBlogPost> {
+        if !validate_post(&post) {
+            return Err(anyhow::anyhow!("new post has invalid data"));
+        }
+
+        let conn = self.pool.get().await?;
+
+        let result = conn.interact(move |conn| -> Result<DBBlogPost> {
+            let now = Local::now();
+            let now_str = now.to_rfc3339();
+            let tags_str = post.tags.join(", ");
+
+            conn.execute(
+                "UPDATE posts SET title = ?1, content = ?2, category = ?3, tags = ?4, updated_at = ?5 WHERE id = ?6",
+                (&post.title, &post.content, &post.category, &tags_str, &now_str, &id)
+            )?;
+
+            Ok(DBBlogPost{
+                id,
+                title: post.title,
+                content: post.content,
+                category: post.category,
+                tags: post.tags,
+                // TODO: created_at should not be updated
+                created_at: now,
+                updated_at: now
+            })
+        }).await.map_err(|e| anyhow::anyhow!("interact error: {}", e))??;
+
+        Ok(result)
     }
 
     async fn delete_post(&self, id: u64) -> Result<()> {
@@ -214,12 +285,20 @@ async fn new_post(
     }
 }
 
-async fn get_post(uri: Uri) -> impl IntoResponse {
-    let (_, id) = uri.path().split_once("/").unwrap();
-    let id: u64 = id.parse().unwrap();
+async fn get_post(State(db): State<Arc<SqliteService>>, uri: Uri) -> impl IntoResponse {
+    let id = uri.path().split("/").last().unwrap();
+    log::info!("get post with id {id}");
+    let id: u64 = id.trim().parse().unwrap();
 
     // TODO: "DB select where id = {id}"
-    StatusCode::INTERNAL_SERVER_ERROR
+    match db.select_post(id).await {
+        Ok(post) => (StatusCode::OK, Json(post)).into_response(),
+        Err(e) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(serde_json::json!({"error": e.to_string()})),
+        )
+            .into_response(),
+    }
 }
 
 async fn get_all_posts(State(db): State<Arc<SqliteService>>) -> impl IntoResponse {
@@ -235,12 +314,24 @@ async fn get_all_posts(State(db): State<Arc<SqliteService>>) -> impl IntoRespons
     }
 }
 
-async fn put_post(uri: Uri, Form(put_post): Form<BlogPost>) -> impl IntoResponse {
-    let (_, id) = uri.path().split_once("/").unwrap();
-    let id: u64 = id.parse().unwrap();
+async fn put_post(
+    State(db): State<Arc<SqliteService>>,
+    uri: Uri,
+    Json(put_post): Json<BlogPost>,
+) -> impl IntoResponse {
+    let id = uri.path().split("/").last().unwrap();
+    log::info!("update post with id {id}");
+    let id: u64 = id.trim().parse().unwrap();
 
     // TODO: "DB update where id = {id}"
-    StatusCode::INTERNAL_SERVER_ERROR
+    match db.update_post(id, put_post).await {
+        Ok(post) => (StatusCode::OK, Json(post)).into_response(),
+        Err(e) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(serde_json::json!({"error": e.to_string()})),
+        )
+            .into_response(),
+    }
 }
 
 async fn delete_post(State(db): State<Arc<SqliteService>>, uri: Uri) -> impl IntoResponse {
--- blog-platform-api/blog-backend/static/script.js
+++ blog-platform-api/blog-backend/static/script.js
@@ -4,19 +4,17 @@ const allPosts = document.getElementById("posts")
 document.getElementById("new-post-form").addEventListener("submit", async (e) => {
     e.preventDefault()
 
-    const title = document.getElementById("new-post-title").value
-    const content = document.getElementById("new-post-content").value
-    const category = document.getElementById("new-post-category").value
-    const tags = document.getElementById("new-post-tags").value.split(", ")
-
-
+    const title = document.getElementById("new-post-title")
+    const content = document.getElementById("new-post-content")
+    const category = document.getElementById("new-post-category")
+    const tags = document.getElementById("new-post-tags")
 
     const response = await fetch("/posts", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ title, content, category, tags }),
+        body: JSON.stringify({ title: title.value, content: content.value, category: category.value, tags: tags.value.split(", ") }),
     })
 
     if (response.status == 201) {
@@ -31,6 +29,11 @@ document.getElementById("new-post-form").addEventListener("submit", async (e) =>
     for (let [key, value] of Object.entries(data)) {
         console.log(`${key}: ${value}`)
     }
+
+    title.value = ""
+    content.value = ""
+    category.value = ""
+    tags.value = ""
 
     posts.appendChild(createPost(data))
 })
@@ -54,33 +57,181 @@ async function fillPosts() {
     });
 }
 
-function createPost(dbBlogPost) {
-    const postContainer = document.createElement("p")
-    postContainer.classList.add("post")
+function findPostById(id) {
+    for (let i = 0; i < allPosts.children.length; i++) {
+        let postId = parseInt(allPosts.children[i].id.split("-")[1])
 
+        if (postId == id) {
+            return allPosts.children[i]
+        }
+    }
+}
+
+function createShowPost(dbBlogPost) {
+    const showContainer = document.createElement("div")
+    showContainer.classList.add("show-post")
+
+    const showTitle = document.createElement("h2")
+    showTitle.innerText = dbBlogPost.title.toString()
+
+    const showContent = document.createElement("p")
+    showContent.innerText = dbBlogPost.content.toString()
+
+    const showCategory = document.createElement("div")
+    showCategory.innerText = dbBlogPost.category.toString()
+
+    const showTags = document.createElement("div")
+    showTags.innerText = dbBlogPost.tags.toString()
+
+    const showDate = document.createElement("div")
+    showDate.innerText = dbBlogPost.created_at.toString()
+
+    showContainer.appendChild(showTitle)
+    showContainer.appendChild(showContent)
+    showContainer.appendChild(showCategory)
+    showContainer.appendChild(showTags)
+    showContainer.appendChild(showDate)
+
+    return showContainer
+}
+
+function createEditButton(dbBlogPost) {
+    const editForm = document.createElement("form")
+    editForm.id = "edit-post-form"
+
+    const editTitle = document.createElement("input")
+    editTitle.type = "text"
+    editTitle.name = "title"
+    editTitle.value = dbBlogPost.title.toString()
+    editTitle.placeholder = "some title"
+    editTitle.id = "edit-post-title"
+
+    const editContent = document.createElement("textarea")
+    editContent.rows = ""
+    editContent.cols = ""
+    editContent.id = "edit-post-content"
+    editContent.value = dbBlogPost.content.toString()
+
+    const editCategory = document.createElement("input")
+    editCategory.type = "text"
+    editCategory.name = "category"
+    editCategory.value = dbBlogPost.category.toString()
+    editCategory.placeholder = "some category (one)"
+    editCategory.id = "edit-post-category"
+
+    const editTags = document.createElement("input")
+    editTags.type = "text"
+    editTags.name = "tags"
+    editTags.value = dbBlogPost.tags.toString()
+    editTags.placeholder = "some tags (separated by comma)"
+    editTags.id = "edit-post-tags"
+
+    const editSave = document.createElement("button")
+    editSave.type = "submit"
+    editSave.id = "edit-post-submit"
+    editSave.innerText = "Save"
+    editSave.addEventListener("click", async (e) => {
+        e.preventDefault()
+
+        const title = document.getElementById("edit-post-title").value
+        const content = document.getElementById("edit-post-content").value
+        const category = document.getElementById("edit-post-category").value
+        const tags = document.getElementById("edit-post-tags").value.split(", ")
+
+        const response = await fetch("/posts/" + dbBlogPost.id.toString(), {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ title, content, category, tags })
+        })
+
+        if (response.status == 200) {
+            alert("Post updated")
+        } else if (response.status == 400) {
+            alert("Something went wrong")
+            return
+        }
+
+        const data = await response.json()
+
+        for (let [key, value] of Object.entries(data)) {
+            console.log(`${key}: ${value}`)
+        }
+
+        const post = findPostById(dbBlogPost.id)
+        post.removeChild(post.children[0])
+        post.appendChild(createPostInfo(data))
+    })
+
+    const editCancel = document.createElement("button")
+    editCancel.type = "submit"
+    editCancel.id = "edit-cancel-post-submit"
+    editCancel.innerText = "Cancel"
+    editCancel.addEventListener("click", (e) => {
+        e.preventDefault()
+        const post = findPostById(dbBlogPost.id)
+
+        post.removeChild(post.children[0])
+        post.appendChild(createPostInfo(dbBlogPost))
+    })
+
+    editForm.appendChild(editTitle)
+    editForm.appendChild(editContent)
+    editForm.appendChild(editCategory)
+    editForm.appendChild(editTags)
+    editForm.appendChild(editSave)
+    editForm.appendChild(editCancel)
+
+    return editForm
+}
+function createPostInfo(dbBlogPost) {
+    const postInfo = document.createElement("div")
 
     const header = document.createElement("h2")
-    const postTitle = document.createElement("a")
+    const postTitle = document.createElement("button")
     postTitle.classList.add("post-title")
-    const postHref = "posts/" + dbBlogPost.id.toString()
-    postTitle.href = postHref
+    postTitle.type = "button"
     postTitle.innerText = dbBlogPost.title.toString()
+    postTitle.addEventListener("click", (e) => {
+        e.preventDefault()
+        const post = findPostById(dbBlogPost.id)
+        post.removeChild(post.children[0])
+        post.appendChild(createShowPost(dbBlogPost))
+
+        const cancelShowButton = document.createElement("button")
+        cancelShowButton.type = "button"
+        cancelShowButton.innerText = "Cancel"
+        cancelShowButton.addEventListener("click", (e) => {
+            e.preventDefault()
+            post.removeChild(post.children[0])
+            post.appendChild(createPostInfo(dbBlogPost))
+        })
+        post.children[0].appendChild(cancelShowButton)
+    })
     header.appendChild(postTitle)
 
-    postContainer.appendChild(header)
+    postInfo.appendChild(header)
 
     const postDate = document.createElement("div")
     postDate.classList.add("post-date")
     postDate.innerText = dbBlogPost.created_at.toString()
 
-    postContainer.appendChild(postDate)
+    postInfo.appendChild(postDate)
 
     const postEdit = document.createElement("button")
     postEdit.type = "button"
     postEdit.classList.add("post-edit")
     postEdit.innerText = "Edit"
+    postEdit.addEventListener("click", (e) => {
+        e.preventDefault()
 
-    postContainer.appendChild(postEdit)
+        const post = findPostById(dbBlogPost.id)
+        post.removeChild(post.children[0])
+        post.appendChild(createEditButton(dbBlogPost))
+    })
+
+    postInfo.appendChild(postEdit)
 
     const postDelete = document.createElement("button")
     postDelete.type = "button"
@@ -98,12 +249,23 @@ function createPost(dbBlogPost) {
             alert("Post was successfully deleted")
         } else {
             alert("Something went wrong " + response.status)
+            return
         }
+        allPosts.removeChild(findPostById(dbBlogPost.id))
 
         // TODO: find deleted post in docuemnt and also delete it
     })
 
-    postContainer.appendChild(postDelete)
+    postInfo.appendChild(postDelete)
+
+    return postInfo
+}
+function createPost(dbBlogPost) {
+    const postContainer = document.createElement("p")
+    postContainer.id = "post-" + dbBlogPost.id
+    postContainer.classList.add("post")
+
+    postContainer.appendChild(createPostInfo(dbBlogPost))
 
     return postContainer
 }
